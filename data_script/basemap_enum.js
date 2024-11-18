8c1a9b286652fb600ed697304fe570f0913a25b1
Louis Vu <loc.vuhuyvn@gmail.com>
2024-11-18 23:37:56 +0700

    Resolve comment

---
+++ data_script/basemap_enum.js
@@ -0,0 +1,70 @@
+const BasemapEnum = {
+	STREETS: {
+		key: "streets",
+		name: "Streets",
+	},
+	STREETS_VECTOR: {
+		key: "streets-vector",
+		name: "Streets Vector",
+	},
+	TOPO: {
+		key: "topo",
+		name: "Topo",
+	},
+	TOPO_VECTOR: {
+		key: "topo-vector",
+		name: "Topo Vector",
+	},
+	GRAY: {
+		key: "gray",
+		name: "Gray",
+	},
+	GRAY_VECTOR: {
+		key: "gray-vector",
+		name: "Gray Vector",
+	},
+	DARK_GRAY: {
+		key: "dark-gray",
+		name: "Dark Gray",
+	},
+	DARK_GRAY_VECTOR: {
+		key: "dark-gray-vector",
+		name: "Dark Gray Vector",
+	},
+	SATELLITE: {
+		key: "satellite",
+		name: "Satellite",
+	},
+	HYBRID: {
+		key: "hybrid",
+		name: "Hybrid",
+	},
+	OCEANS: {
+		key: "oceans",
+		name: "Oceans",
+	},
+	NATIONAL_GEOGRAPHIC: {
+		key: "national-geographic",
+		name: "National Geographic.",
+	},
+	TERRAIN: {
+		key: "terrain",
+		name: "Terrain",
+	},
+	STREETS_RELIEF_VECTOR: {
+		key: "streets-relief-vector",
+		name: "Streets Relief Vector",
+	},
+	NAVIGATION_VECTOR: {
+		key: "navigation-vector",
+		name: "Navigation Vector",
+	},
+	STREETS_NIGHT_VECTOR: {
+		key: "streets-night-vector",
+		name: "Streets Night Vector",
+	},
+	OUTDOORS: {
+		key: "outdoors",
+		name: "Outdoors",
+	},
+};
