87d31ae2eb4a703ce047d64b03d9e0ed8f0399a8
liemvh1998 <35839746+liemvh1998@users.noreply.github.com>
2024-12-10 22:52:44 +0700

    - add 3 locations (#14)

--- data_script/locations.js
+++ data_script/locations.js
@@ -44,6 +44,51 @@ const locations = [
 			},
 		},
 	},
+	{
+		type: 'point',
+		longitude: 106.68142535262977,
+		latitude: 10.786341711256426,
+		name: 'Chùa Pháp Hoa',
+		location: '870 Đường Trường Sa, Phường 14, Quận 3, Hồ Chí Minh 700000, Việt Nam',
+		symbol: {
+		 	type: 'simple-marker',
+		 	color: [137, 204, 143],
+		 	outline: {
+		  		color: [137, 165, 160],
+		  		width: 1,
+		 	},
+		},
+	},
+	{
+		type: 'point',
+		longitude: 106.67197891058952,
+		latitude: 10.789108886725948, 
+		name: 'Nhà Thờ Giáo Xứ Bùi Phát',
+		location: 'Hẻm 1074 Đ. Trường Sa, Phường 12, Quận 3, Hồ Chí Minh, Việt Nam',
+		symbol: {
+		 	type: 'simple-marker',
+		 	color: [237, 104, 143],
+		 	outline: {
+		  		color: [137, 165, 160],
+		  		width: 1,
+		 	},
+		},
+	},
+	{
+		type: 'point',
+		longitude: 106.67288012795663, 
+		latitude: 10.78700070305095, 
+		name: 'Nhà thờ An Phú',
+		location: '205/45 Đ. Đỗ Thị Lời, Phường 11, Quận 3, Hồ Chí Minh, Việt Nam',
+		symbol: {
+		 	type: 'simple-marker',
+		 	color: [137, 164, 43],
+		 	outline: {
+		  		color: [137, 165, 160],
+		  		width: 1,
+		 	},
+		},
+	},
 ];
 
 let customLocationContent = function (feature) {
